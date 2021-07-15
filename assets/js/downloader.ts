4e7cfcf3ae78427ba4fea312c5e58e0b815b3b8b
andrewgremlich <andrew.gremlich@gmail.com>
2021-07-15 11:13:40 -0600

    Updating how to fetch posts from cache

--- assets/js/downloader.ts
+++ assets/js/downloader.ts
@@ -3,42 +3,48 @@ import { trash, download } from "./mod/elements";
 trash.style.display = "none";
 download.style.display = "none";
 
-// refer to /sw.js for cache name.
-const APP_CACHE = caches.open("blog_cache");
+caches.keys().then((keys) => {
+  keys.forEach((key) => {
+    if (key.includes("_blog_")) {
+      // refer to /sw.js for cache name.
+      const APP_CACHE = caches.open(key);
 
-download.addEventListener("click", (evt) => {
-  APP_CACHE.then((cache) => {
-    cache.add(window.location.pathname);
-    trash.style.display = "block";
-    download.style.display = "none";
-  });
-});
+      download.addEventListener("click", (evt) => {
+        APP_CACHE.then((cache) => {
+          cache.add(window.location.pathname);
+          trash.style.display = "block";
+          download.style.display = "none";
+        });
+      });
 
-trash.addEventListener("click", (evt) => {
-  APP_CACHE.then((cache) => {
-    const CACHE_KEYS = cache.keys();
-    CACHE_KEYS.then((keyList) => {
-      const itemToDelete = keyList.find(
-        (key) => key.url === window.location.href
-      );
-      cache.delete(itemToDelete);
-      trash.style.display = "none";
-      download.style.display = "block";
-    });
-  });
-});
+      trash.addEventListener("click", (evt) => {
+        APP_CACHE.then((cache) => {
+          const CACHE_KEYS = cache.keys();
+          CACHE_KEYS.then((keyList) => {
+            const itemToDelete = keyList.find(
+              (key) => key.url === window.location.href
+            );
+            cache.delete(itemToDelete);
+            trash.style.display = "none";
+            download.style.display = "block";
+          });
+        });
+      });
 
-APP_CACHE.then((cache) => {
-  const CACHE_KEYS = cache.keys();
-  CACHE_KEYS.then((keyList) => {
-    const existPageInCache = keyList.find(
-      (key) => key.url === window.location.href
-    );
+      APP_CACHE.then((cache) => {
+        const CACHE_KEYS = cache.keys();
+        CACHE_KEYS.then((keyList) => {
+          const existPageInCache = keyList.find(
+            (key) => key.url === window.location.href
+          );
 
-    if (existPageInCache) {
-      trash.style.display = "block";
-    } else {
-      download.style.display = "block";
+          if (existPageInCache) {
+            trash.style.display = "block";
+          } else {
+            download.style.display = "block";
+          }
+        });
+      });
     }
   });
 });
